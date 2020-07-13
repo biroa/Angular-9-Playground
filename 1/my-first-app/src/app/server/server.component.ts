e19bd7c11130a693cb02e92a7f7d39e2f176728e
a.biro <a.biro@fbs.com>
2020-07-13 14:57:52 +0100

    code of the first project

---
+++ 1/my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+// We need to import to component to make the component understandable
+import {Component} from '@angular/core';
+
+// Add decorator to enhance our code
+@Component({
+  selector: 'app-server', // it should be a unique
+  templateUrl: './server.component.html'
+})
+export class ServerComponent {
+
+}
---
+++ 1/my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // Selector name should unique
+  //selector: 'app-servers', // This is selector by tag
+  // selector: '[app-servers]', // This is selector by tag
+  // tslint:disable-next-line:component-selector
+  selector: '.app-servers',
+  /* Template or TemplateUrl must be present
+  Option 1:
+  template: '<app-server></app-server><app-server></app-server>',
+  Option 2:
+  template:`
+    <app-server></app-server>
+    <app-server></app-server>
+  `,
+  Option 3:
+  */
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
