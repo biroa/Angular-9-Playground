30f8c31f393d97f59d0f659595495299615f49d4
a.biro <a.biro@fbs.com>
2020-12-02 14:38:03 +0000

    Data Binding; Component Communication; EventEmitter;@Input - @Output Decorators; View encapsulation; Local References in the template

---
+++ databinding/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,43 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+// @ts-ignore
+export class CockpitComponent implements OnInit {
+  // Set Listenable with @Output
+  // @ts-ignore
+  @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  // tslint:disable-next-line:no-output-rename
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+     // If you want use the Two-Way Data binding approach comment it out
+     // But we converted the code to a local reference approach
+     // newServerName = '';
+     newServerContent = '';
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  // tslint:disable-next-line:typedef
+  onAddServer(serverNameInput: HTMLInputElement) {
+    this.serverCreated.emit({
+        serverName: serverNameInput.value,
+        serverContent: this.newServerContent
+      }
+    );
+  }
+  // tslint:disable-next-line:typedef
+  onAddBlueprint(serverNameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({
+        serverName: serverNameInput.value,
+        serverContent: this.newServerContent
+      }
+    );
+  }
+
+}
