a8217eb82c09cae7b972b14bd988e6be89c30aaa
a.biro <a.biro@fbs.com>
2020-12-21 17:42:04 +0000

    custom structural directive

--- directives/src/app/basic-highlight/basic-highlight.directive.ts
+++ directives/src/app/basic-highlight/basic-highlight.directive.ts
@@ -7,7 +7,7 @@ export class BasicHighlightDirective implements OnInit {
   constructor(private elementRef: ElementRef) {
   }
   ngOnInit() {
-    // This is not an ideal approach to directlz access Dom Element This Way
+    // This is not an ideal approach to directly access Dom Element This Way
     this.elementRef.nativeElement.style.backgroundColor = 'red';
     this.elementRef.nativeElement.style.color = 'white';
     this.elementRef.nativeElement.style.fontWeight = 'bold';
--- directives/src/app/custom/unless.directive.ts
+++ directives/src/app/custom/unless.directive.ts
@@ -1,10 +1,34 @@
-import { Directive } from '@angular/core';
+import {Directive, Input, TemplateRef, ViewContainerRef} from '@angular/core';
 
 @Directive({
   selector: '[appUnless]'
 })
 export class UnlessDirective {
 
-  constructor() { }
+  /*
+    With the usage of the set keyword we want to execute the setter method
+    of the property. When changes happens we want to execute
+    the method
+  */
+
+  @Input() set appUnless(condition: boolean) {
+    if (!condition) {
+      this.vcRef.createEmbeddedView(this.templateRef);
+    } else {
+      this.vcRef.clear();
+    }
+  }
+
+  /**
+   *
+   * @param templateRef - What we render
+   * @param vcRef - Where we render it
+   * @param ViewContainerRef
+   */
+  constructor(
+    private templateRef: TemplateRef<any>,
+    private vcRef: ViewContainerRef) {
+
+  }
 
 }
